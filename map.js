19d5fd5d497ec24312f47a7da8fa47afbea4dd4b
takyebin <yeebin112@gmail.com>
2024-06-20 20:38:08 +0900

    Delete map.js

--- map.js
+++
@@ -1,9 +0,0 @@
-var map = L.map('map').setView([37.5665, 126.9780], 12);
-
-L.tileLayer('https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png', {
-    attribution: '&copy; <a href="https://www.openstreetmap.org/copyright">OpenStreetMap</a> contributors'
-}).addTo(map);
-
-L.marker([37.5665, 126.9780]).addTo(map)
-    .bindPopup('Seoul')
-    .openPopup();
